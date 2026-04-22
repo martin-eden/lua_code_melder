a7f8f2f3194ff03944446f1579c8b8c904368e5d
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2026-04-22 20:12:51 +0200

    [^] [meld] Code styling: change to snake_case..
    .. except for names of tables and table fields which are CamelCase.

--- src/meld.lua
+++ src/meld.lua
@@ -1,4 +1,4 @@
--- Aggregate Lua source files to one
+-- Aggregate Lua source files in current directory to one
 
 --[[
   Author: Martin Eden
@@ -15,119 +15,134 @@ require('workshop.base')
 --]]
 
 local FilesLister = request('!.concepts.FilesLister.Interface')
-local TableToCodeStr = request('!.table.as_string')
-local FileAsString = request('!.file_system.file.as_string')
-local Lines = request('!.concepts.Lines.Interface')
-local StringToLines = request('!.string.to_lines')
 local ParsePathName = request('!.concepts.path_name.parse')
+local file_as_string = request('!.file_system.file.as_string')
+local Lines = request('!.concepts.Lines.Interface')
+local table_to_string = request('!.table.as_string')
 
-local LooksLikeModule =
-  function(FileName)
-    local LuaFileRegexp = '%.lua$'
-    if string.find(FileName, LuaFileRegexp) then
+local looks_like_module =
+  function(file_name)
+    local lua_file_regexp = '%.lua$'
+    if string.find(file_name, lua_file_regexp) then
       return true
     end
     return false
   end
 
-local GetModuleName =
-  function(PathName)
-    local ParsedName = ParsePathName(PathName)
-    local DirName = ParsedName.directory
-    local FileName = ParsedName.name
+-- Convert pathname to Lua's require() module name
+local get_module_name =
+  function(path_name)
+    local ParsedName = ParsePathName(path_name)
+    local dir_name = ParsedName.directory
+    local file_name = ParsedName.name
     assert(not ParsedName.is_directory)
 
-    local ModuleName
+    local module_name
     -- Module name is file name without ".lua" at end
-    local ModuleNameRegexp = '(.*)%.lua$'
-    ModuleName = string.match(FileName, ModuleNameRegexp)
+    local module_name_capture = '(.*)%.lua$'
+    module_name = string.match(file_name, module_name_capture)
 
-    -- if we are in directory then
-    if (DirName ~= '') then
-      -- Module name is prefixed with directory path with "/" replaced to "."
-      local ModuleNamePrefix = string.gsub(DirName, '/', '.')
-      ModuleName = ModuleNamePrefix .. ModuleName
+    -- If we are in directory then ..
+    if (dir_name ~= '') then
+      -- .. module name is prefixed with directory path with "/" replaced to "."
+      local module_name_prefix = string.gsub(dir_name, '/', '.')
+      module_name = module_name_prefix .. module_name
     end
 
-    return ModuleName
+    return module_name
   end
 
-local PopulateModules
-PopulateModules =
-  function(DirName, Result)
-    FilesLister:SetBaseDirectory(DirName)
+--[[
+  Files visitor
+
+  For each Lua file in current directory:
+
+    * Gets it's name as module name (for require())
+    * Gets it's contents
+    * Adds to Result table: Result[module_name] = file_contents
+]]
+local populate_modules
+populate_modules =
+  function(base_dir_name, Result)
+    FilesLister:SetBaseDirectory(base_dir_name)
 
     local Files = FilesLister:GetFiles()
-    for _, FileName in ipairs(Files) do
-      if LooksLikeModule(FileName) then
-        local FullFileName = DirName .. FileName
-        local ModuleCode = FileAsString(FullFileName)
-        local ModuleName = GetModuleName(FullFileName)
-        Result[ModuleName] = ModuleCode
+
+    for _, file_name in ipairs(Files) do
+      if looks_like_module(file_name) then
+        local full_file_name = base_dir_name .. file_name
+        local file_contents = file_as_string(full_file_name)
+        local module_name = get_module_name(full_file_name)
+        Result[module_name] = file_contents
       end
     end
 
     local Directories = FilesLister:GetDirectories()
-    for _, Directory in ipairs(Directories) do
-      PopulateModules(DirName .. Directory, Result)
+
+    for _, subdir_name in ipairs(Directories) do
+      populate_modules(base_dir_name .. subdir_name, Result)
     end
   end
 
-local GetModules =
+local get_modules =
   function()
     local Result = {}
-    PopulateModules('./', Result)
+    populate_modules('./', Result)
     return Result
   end
 
-local Modules = GetModules()
-local ModulesTableStr = TableToCodeStr(Modules)
-Lines:FromString(ModulesTableStr)
+local Modules = get_modules()
+local modules_table_str = table_to_string(Modules)
+Lines:FromString(modules_table_str)
 
-local AddModulesTablePrefix =
+-- Start first line in table definition string from "local Modules = " ..
+local add_modules_table_prefix =
   function(Lines)
-    local FirstLine = Lines:GetFirstLine()
-    assert(FirstLine == '{')
-    FirstLine = 'local Modules = ' .. FirstLine
+    local first_line = Lines:GetFirstLine()
+    assert(string.sub(first_line, 1, 1) == '{')
+    first_line = 'local Modules = ' .. first_line
     Lines:RemoveFirstLine()
-    Lines:AddFirstLine(FirstLine)
+    Lines:AddFirstLine(first_line)
   end
 
-local AddModulesPopulationCode =
+local add_modules_population_code =
   function(Lines)
-    local PopulationCode = [[
-local AddModule =
-  function(Name, Code)
-    local CompiledCode = assert(load(Code, Name, 't'))
+    local code_str = [[
+local add_module =
+  function(module_name, module_code_str)
+    local compiled_code = assert(load(module_code_str, module_name, 't'))
 
-    _G.package.preload[Name] =
+    _G.package.preload[module_name] =
       function(...)
-        return CompiledCode(...)
+        return compiled_code(...)
       end
   end
 
-for ModuleName, ModuleCode in pairs(Modules) do
-  AddModule(ModuleName, ModuleCode)
+for module_name, module_code_str in pairs(Modules) do
+  add_module(module_name, module_code_str)
 end]]
-    Lines:AddLastLine(PopulationCode)
+    Lines:AddLastLine(code_str)
   end
 
-local AddModuleCall =
-  function(ModuleName)
-    local ModuleCallFmt = "require('%s')"
-    local ModuleCallStr = string.format(ModuleCallFmt, ModuleName)
-    Lines:AddLastLine(ModuleCallStr)
+local add_module_call =
+  function(module_name)
+    local module_call_str
+
+    if is_string(module_name) then
+      local module_call_fmt = "require('%s')"
+      module_call_str = string.format(module_call_fmt, module_name)
+    else
+      module_call_str = 'require(arg[1])'
+    end
+
+    Lines:AddLastLine(module_call_str)
   end
 
-AddModulesTablePrefix(Lines)
+add_modules_table_prefix(Lines)
 Lines:AddLastLine('')
-AddModulesPopulationCode(Lines)
+add_modules_population_code(Lines)
 Lines:AddLastLine('')
-if arg[1] then
-  AddModuleCall(arg[1])
-else
-  Lines:AddLastLine('require(arg[1])')
-end
+add_module_call(arg[1])
 
 io.write(Lines:ToString())
 
