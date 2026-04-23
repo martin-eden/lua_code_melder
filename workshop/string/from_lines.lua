0f68586fcf0cc20e092b8aaba3f941e7da0b871d
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2026-04-23 18:26:08 +0200

    [>] Snapshot of [workshop].

---
+++ workshop/string/from_lines.lua
@@ -0,0 +1,37 @@
+-- Concatenate sequence of strings
+
+--[[
+  Author: Martin Eden
+  Last mod.: 2026-04-17
+]]
+
+--[[
+  Concatenate strings with newline separator. Tailing newline.
+]]
+local <const> LinesToString =
+  function(Lines)
+    assert_table(Lines)
+
+    local Result
+    local <const> Separator = '\n'
+
+    Result = table.concat(Lines, Separator)
+    Result = Result .. Separator
+
+    return Result
+  end
+
+-- Export:
+return LinesToString
+
+--[[
+  Good design is not about implementing complex things.
+  Good design is providing uniform interface to many things.
+
+  2024-02-28, 2024-03-02
+]]
+
+--[[
+  2024-02-28
+  2026-04-17
+]]
