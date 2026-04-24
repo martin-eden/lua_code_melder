650acf98350e3ccc41d3ef6d0ab8908a20f5dcfa
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2026-04-24 10:33:20 +0200

    [!] [meld] Finished direct source injection.

--- meld.lua
+++ meld.lua
@@ -6,7 +6,7 @@
 ]]
 
 --[[
-  To do
+  Done
 
     * Embed source contents directly into package.preload
 
@@ -54,9 +54,11 @@ local FilesLister = request('!.concepts.FilesLister.Interface')
 local parse_path_name = request('!.concepts.path_name.parse')
 local file_as_string = request('!.file_system.file.as_string')
 local table_to_string = request('!.table.as_string')
-local Lines = request('!.concepts.Lines.Interface')
+local LinesClass = request('!.concepts.Lines.Interface')
 local string_starts_with = request('!.string.starts_with')
 local string_ends_with = request('!.string.ends_with')
+local ordered_pairs = request('!.table.ordered_pass')
+local lua_quote_string = request('!.concepts.lua.quote_string')
 
 local is_lua_file =
   function(file_name)
@@ -122,35 +124,21 @@ local get_modules =
     return Result
   end
 
--- Start first line in table definition string from "local Modules = " ..
-local add_modules_table_prefix =
-  function(Lines)
-    local first_line = Lines:GetFirstLine()
-    assert(string_starts_with(first_line, '{'))
-    first_line = 'local Modules = ' .. first_line
-    Lines:RemoveFirstLine()
-    Lines:AddFirstLine(first_line)
-  end
+local add_module_registration =
+  function(Lines, module_name, module_code)
+    local quoted_module_name = lua_quote_string(module_name)
 
-local add_modules_population_code =
-  function(Lines)
-    local code_str = [[
-do
-  local add_module =
-    function(module_name, module_code_str)
-      local compiled_code = assert(load(module_code_str, module_name, 't'))
+    local ModuleLines = new(LinesClass)
+    ModuleLines:FromString(module_code)
+    ModuleLines:Indent()
+    ModuleLines:Indent()
 
-      _G.package.preload[module_name] =
-        function(...)
-          return compiled_code(...)
-        end
-    end
+    local indented_module_code = ModuleLines:ToString()
 
-  for module_name, module_code_str in pairs(Modules) do
-    add_module(module_name, module_code_str)
-  end
-end]]
-    Lines:AddLastLine(code_str)
+    Lines:AddLastLine('_G.package.preload[' .. quoted_module_name .. '] =')
+    Lines:AddLastLine('  function(...)')
+    Lines:AddLastLine(indented_module_code)
+    Lines:AddLastLine('  end')
   end
 
 local add_module_call =
@@ -170,12 +158,15 @@ do
     return
   end
 
-  Lines:FromString(table_to_string(get_modules(Config.ModulesDir)))
+  local Modules = get_modules(Config.ModulesDir)
 
-  add_modules_table_prefix(Lines)
-  Lines:AddLastLine('')
-  add_modules_population_code(Lines)
-  Lines:AddLastLine('')
+  local Lines = new(LinesClass)
+
+  for module_name, module_code in ordered_pairs(Modules) do
+    add_module_registration(Lines, module_name, module_code)
+    Lines:AddLastLine('')
+  end
+
   add_module_call(Lines, Config.RootModule)
 
   io.write(Lines:ToString())
