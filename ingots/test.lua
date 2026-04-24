0054aab660edef975ab64765f9ad621f9f7d31be
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2026-04-24 10:22:50 +0200

    [~] [ingots.test]

--- ingots/test.lua
+++ ingots/test.lua
@@ -1,47 +1,27 @@
-local Modules = {
-  ['modules.a'] = [[
-local Represent = require('modules.a.Represent')
+_G.package.preload['modules.a'] =
+  function(...)
+    local Represent = require('modules.a.Represent')
 
-return
-  {
-    Represent = Represent,
-    Data = '[Test representation.]',
-  }
-]],
-  ['modules.a.Represent'] = [[
-return
-  function(self)
-    print(self.Data)
+    return
+      {
+        Represent = Represent,
+        Data = '[Test representation.]',
+      }
   end
-]],
-  test = [[
--- Test code for melder
 
-local ModuleA = require('modules.a')
-
-if not ModuleA then
-  print('Failed.')
-  return
-end
-
-ModuleA:Represent()
-]],
-}
-
-do
-  local add_module =
-    function(module_name, module_code_str)
-      local compiled_code = assert(load(module_code_str, module_name, 't'))
-
-      _G.package.preload[module_name] =
-        function(...)
-          return compiled_code(...)
-        end
-    end
-
-  for module_name, module_code_str in pairs(Modules) do
-    add_module(module_name, module_code_str)
+_G.package.preload['modules.a.Represent'] =
+  function(...)
+    return
+      function(self)
+        print(self.Data)
+      end
   end
-end
+
+_G.package.preload['test'] =
+  function(...)
+    local ModuleA = require('modules.a')
+
+    ModuleA:Represent()
+  end
 
 require('test')
