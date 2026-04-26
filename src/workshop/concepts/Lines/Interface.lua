501f245dbba0a28db1a6f2dad56c6901dbc8f50e
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2026-04-26 19:14:38 +0200

    [>] [workshop]

--- src/workshop/concepts/Lines/Interface.lua
+++ src/workshop/concepts/Lines/Interface.lua
@@ -1,101 +1,190 @@
--- List of lines interface
+-- List of lines interface. Frontend
 
 --[[
   Author: Martin Eden
-  Last mod.: 2026-04-17
+  Last mod.: 2026-04-26
 ]]
 
--- "Line" is a string with newline. "String" is a line without newline.
-
 --[[
-  This class is about storing list of lines.
+  This class is about storing list of lines
 
   Lines are stored as strings (without newlines).
 
-  Text output facilities work with lines, not with multiline
-  strings. Among other things they want indentation. We're
-  providing indentation. They want add header or footer. We're
-  providing that.
+  Also we're providing convenience methods for indentation.
 ]]
 
--- Exports:
+local Core = request('Core.Interface')
+local Indenter = request('Indent-Methods')
+
+-- ( Core wrappers
+
+-- String to lines list
+local FromString =
+  function(Me, str)
+    local Core = Me.Core
+
+    Core:FromString(str)
+  end
+
+-- Lines to string
+local ToString =
+  function(Me)
+    local Core = Me.Core
+
+    return Core:ToString()
+  end
+
+-- Return number of lines
+local GetNumLines =
+  function(Me)
+    local Core = Me.Core
+
+    return Core:GetNumItems()
+  end
+
+-- Return line's contents by index
+local GetLineAt =
+  function(Me, index)
+    local Core = Me.Core
+
+    Core:AssertValidIndex(index)
+
+    return Core:GetItem(index)
+  end
+
+-- Set line at given index
+local SetLineAt =
+  function(Me, str, index)
+    local Core = Me.Core
+
+    Core:AssertValidValue(str)
+    Core:AssertValidIndex(index)
+
+    Core:SetItem(Core:ToItem(str), index)
+  end
+
+-- Insert line before given index
+local InsertLineBefore =
+  function(Me, str, index)
+    local Core = Me.Core
+
+    Core:AssertValidValue(str)
+    Core:AssertValidIndex(index)
+
+    Core:InsertItemBefore(Core:ToItem(str), index)
+  end
+
+-- Insert line after given index
+local InsertLineAfter =
+  function(Me, str, index)
+    local Core = Me.Core
+
+    Core:AssertValidValue(str)
+    Core:AssertValidIndex(index)
+
+    Core:InsertItemBefore(Core:ToItem(str), index + 1)
+  end
+
+-- Remove line at given index
+local RemoveLineAt =
+  function(Me, index)
+    local Core = Me.Core
+
+    Core:AssertValidIndex(index)
+
+    Core:RemoveItemAt(index)
+  end
+
+-- ) Core wrappers
+
+-- ( Extensions
+
+-- Return true when we have just one line
+local IsOneLine =
+  function(Me)
+    return (Me:GetNumLines() == 1)
+  end
+
+-- Return first line
+local GetFirstLine =
+  function(Me)
+    return Me:GetLineAt(1)
+  end
+
+-- Return last line
+local GetLastLine =
+  function(Me)
+    return Me:GetLineAt(Me:GetNumLines())
+  end
+
+-- Add line to start of list
+local AddFirstLine =
+  function(Me, str)
+    Me:InsertLineBefore(Me.Core:ToItem(str), 1)
+  end
+
+-- Add string to end of list
+local AddLastLine =
+  function(Me, str)
+    Me:InsertLineAfter(Me.Core:ToItem(str), Me:GetNumLines())
+  end
+
+-- Remove first line
+local RemoveFirstLine =
+  function(Me)
+    Me:RemoveLineAt(1)
+  end
+
+-- Remove last line
+local RemoveLastLine =
+  function(Me)
+    Me:RemoveLineAt(Me:GetNumLines())
+  end
+
+-- ) Extensions
+
+-- Export:
 return
   {
     -- Import/export:
-
-    -- Import: Explode string to list of lines
-    FromString = request('FromString'),
-
-    -- Export: Implode list of lines to string
-    ToString = request('ToString'),
+    FromString = FromString,
+    ToString = ToString,
 
     -- Access:
 
-    -- Meta: get number lines
-    GetNumLines = request('GetNumLines'),
+    GetNumLines = GetNumLines,
 
-    -- MetaMeta: is one line?
-    IsOneLine = request('IsOneLine'),
+    IsOneLine = IsOneLine,
 
-    -- Access: get string at given line
-    GetLineAt = request('GetLineAt'),
+    GetLineAt = GetLineAt,
 
-    -- Access: set string at given line
-    SetLineAt = request('SetLineAt'),
+    GetFirstLine = GetFirstLine,
+    GetLastLine = GetLastLine,
 
-    -- Meta: get first line string
-    GetFirstLine = request('GetFirstLine'),
+    SetLineAt = SetLineAt,
 
-    -- Meta: get last line string
-    GetLastLine = request('GetLastLine'),
+    InsertLineBefore = InsertLineBefore,
+    InsertLineAfter = InsertLineAfter,
 
-    -- Modification:
+    AddFirstLine = AddFirstLine,
+    AddLastLine = AddLastLine,
 
-    -- Add string to start of list
-    AddFirstLine = request('AddFirstLine'),
+    RemoveLineAt = RemoveLineAt,
 
-    -- Add string to end of list
-    AddLastLine = request('AddLastLine'),
-
-    -- Insert string before line at given index
-    InsertLineBefore = request('InsertLineBefore'),
-
-    -- Insert string after line at given index
-    InsertLineAfter = request('InsertLineAfter'),
-
-    -- Remove first line
-    RemoveFirstLine = request('RemoveFirstLine'),
-
-    -- Remove last line
-    RemoveLastLine = request('RemoveLastLine'),
-
-    -- Remove line
-    RemoveLineAt = request('RemoveLineAt'),
+    RemoveFirstLine = RemoveFirstLine,
+    RemoveLastLine = RemoveLastLine,
 
     -- Indentation:
-
-    -- Indent
-    Indent = request('Indent'),
-
-    -- Unindent
-    Unindent = request('Unindent'),
+    Indent = Indenter.Indent,
+    Unindent = Indenter.Unindent,
 
     -- Internals:
-
-    -- Internal: list of lines
-    Lines = {},
-
-    -- Config: indent chunk
-    IndentChunk = '  ',
-
-    -- Internal: assert that value is valid index
-    AssertValidIndex = request('AssertValidIndex'),
-
-    -- Internal: assert that value is string without newlines
-    AssertValidValue = request('AssertValidValue'),
+    Core = Core,
   }
 
 --[[
-  2024-10-31
+  2024 #
   2026-04-17
+  2026-04-24
+  2026-04-26
 ]]
