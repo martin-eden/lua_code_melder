d2d46757c88010ef25350c1103a84db6af7cb6a6
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2026-06-16 16:06:15 +0200

    [~] [workshop]

---
+++ src/workshop/concepts/lua/NumberTypeNames.lua
@@ -0,0 +1,20 @@
+-- Return list of numeric type names
+
+--[[
+  Author: Martin Eden
+  Last mod.: 2026-06-16
+]]
+
+local NumberTypeNames =
+  {
+    'integer',
+    'float',
+  }
+
+-- Export:
+return NumberTypeNames
+
+--[[
+  2024-03-02
+  2026-06-16
+]]
