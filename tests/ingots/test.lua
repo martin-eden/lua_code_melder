37a465a822e9f4e2b534bbcdde19a632e3173beb
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2026-06-16 12:58:46 +0200

    [~] [test]

--- tests/ingots/test.lua
+++ tests/ingots/test.lua
@@ -1,27 +1,33 @@
-_G.package.preload['modules.a'] =
-  function(...)
-    local Represent = require('modules.a.Represent')
-
-    return
-      {
-        Represent = Represent,
-        Data = '[Test representation.]',
-      }
-  end
-
-_G.package.preload['modules.a.Represent'] =
-  function(...)
-    return
-      function(self)
-        print(self.Data)
-      end
-  end
-
+-- ( ( module test )
 _G.package.preload['test'] =
-  function(...)
-    local ModuleA = require('modules.a')
+function(...)
+local ModuleA = require('modules.a')
 
-    ModuleA:Represent()
+ModuleA:Represent()
+end
+-- ) ( module test )
+
+-- ( ( module modules.a )
+_G.package.preload['modules.a'] =
+function(...)
+local Represent = require('modules.a.Represent')
+
+return
+  {
+    Represent = Represent,
+    Data = '[Test representation.]',
+  }
+end
+-- ) ( module modules.a )
+
+-- ( ( module modules.a.Represent )
+_G.package.preload['modules.a.Represent'] =
+function(...)
+return
+  function(self)
+    print(self.Data)
   end
+end
+-- ) ( module modules.a.Represent )
 
 require('test')
